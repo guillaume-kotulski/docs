f351349361881deae0dacac748997cfcf55204a8
arnaud4d <arnaud.schmitt@4d.com>
2020-06-25 17:29:41 +0200

    fixed baseURL

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -26,7 +26,7 @@ const siteConfig = {
   url: 'https://4d.github.io' /* your website url */,
   //baseUrl: '/doc/' /* base url for your project */,
   //baseUrl: '/docs/' /* base url for your project */,
-  baseUrl: '/docs/' /* base url for your project */,
+  baseUrl: '/docs/Rx/' /* base url for your project */,
 // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
@@ -38,7 +38,7 @@ const siteConfig = {
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
-  docsUrl:'/docs/Rx/',
+  docsUrl:'',
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
