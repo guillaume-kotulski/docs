803393a133205e8605b8e3b054af7c3f29942402
arnaud4d <arnaud.schmitt@4d.com>
2020-08-11 17:05:59 +0200

    temp

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -80,7 +80,7 @@ class Index extends React.Component {
                   content: `[${subContents.projectDatabases}](${siteConfig.baseUrl}${this.props.language}/Project/overview.html)<br>
                   [${subContents.languageConcepts}](${siteConfig.baseUrl}${this.props.language}/Concepts/about.html)<br>
 				  [${subContents.langRef}](https://doc.4d.com/4Dv18R4/4D/18-R4/4D-Language-Reference.100-4981854.en.html)<br>
-                  [${subContents.ORDA}](${siteConfig.baseUrl}${this.props.language}/ORDA/ordaClasses.html)<br>
+                  [${subContents.ORDA}](${siteConfig.baseUrl}${this.props.language}/ORDA/overview.html)<br>
                   [${subContents.class}](${siteConfig.baseUrl}${this.props.language}/API/cryptoClass.html)<br>
                   [${subContents.users}](${siteConfig.baseUrl}${this.props.language}/Users/overview.html)`,
                   image: `${siteConfig.baseUrl}img/illu_CoreDevelopment.png`,
